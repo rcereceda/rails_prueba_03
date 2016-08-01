6f5185af83247d93b0f55665b3e593bd25e2b6cc
Rodrigo Cereceda <rcereced@gmail.com>
2016-08-01 19:24:34 -0300

    models & devise

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,6 @@
+class SongsController < ApplicationController
+	before_action :authenticate_user!
+
+  def index
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :user_songs
+  has_many :songs, through: :user_songs
+end
